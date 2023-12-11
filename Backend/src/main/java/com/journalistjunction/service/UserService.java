4343f241eb21071671ae4f902a0a097567b559d5
RaresButuc <eusuntbutuc01@gmail.com>
2023-12-11 12:54:08 +0200

    UserService Class

---
+++ Backend/src/main/java/com/journalistjunction/service/UserService.java
@@ -0,0 +1,37 @@
+package com.journalistjunction.service;
+
+import com.journalistjunction.model.User;
+import com.journalistjunction.repository.UserRepository;
+
+import java.util.List;
+
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void updateUserById(Long id, User updatedUser) {
+        User userFromDb = userRepository.findById(id).orElse(null);
+        assert userFromDb != null;
+        userFromDb.setName(updatedUser.getName());
+        userFromDb.setEmail(updatedUser.getEmail());
+        userFromDb.setPhoneNumber(updatedUser.getPhoneNumber());
+        userRepository.save(userFromDb);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
