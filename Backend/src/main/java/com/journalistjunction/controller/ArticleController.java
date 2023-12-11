cea6d48444028f6409e49697574e71a02e532ddb
RaresButuc <eusuntbutuc01@gmail.com>
2023-12-11 13:45:53 +0200

    ArticleController Class

---
+++ Backend/src/main/java/com/journalistjunction/controller/ArticleController.java
@@ -0,0 +1,42 @@
+package com.journalistjunction.controller;
+
+import com.journalistjunction.model.Article;
+import com.journalistjunction.service.ArticleService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/articles")
+public class ArticleController {
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping
+    public List<Article> getAllArticles() {
+        return articleService.getAllArticles();
+    }
+
+    @GetMapping("/{id}")
+    public Article getArticleById(@PathVariable("id") Long id) {
+        return articleService.getArticleById(id);
+    }
+
+    @PostMapping
+    public void postNewArticle(Article article) {
+        articleService.addArticle(article);
+    }
+
+    @PutMapping("/{id}")
+    public void editArticle(@PathVariable("id") Long id, Article articleEdited) {
+        articleService.updateArticleById(id, articleEdited);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteArticle(@PathVariable("id") Long id) {
+        articleService.deleteArticleById(id);
+    }
+}
