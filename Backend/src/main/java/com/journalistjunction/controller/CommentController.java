2dc0ed75f80710bcdd6f35bf087f98020d0f23dc
RaresButuc <eusuntbutuc01@gmail.com>
2023-12-12 14:59:34 +0200

    Fix RequestMapping Comment

--- Backend/src/main/java/com/journalistjunction/controller/CommentController.java
+++ Backend/src/main/java/com/journalistjunction/controller/CommentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/photo")
+@RequestMapping(value = "/comment")
 public class CommentController {
     private final CommentService commentService;
 
