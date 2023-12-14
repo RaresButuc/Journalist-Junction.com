886381249cea778b6cd53114174ae6d8f312fc13
RaresButuc <eusuntbutuc01@gmail.com>
2023-12-14 18:04:19 +0200

    Categories designed and phone responsive

--- Frontend/src/components/TopNews.js
+++ Frontend/src/components/TopNews.js
@@ -5,7 +5,7 @@ export default function TopNews() {
     id: 1,
     title: "Titlu de Articol",
     short:
-      "The decision to freeze the case came as former President Trump’s lawyers asked a court to move slowly in reviewing his claims of presidential immunity.",
+      "The decision to freeze the case came as former President Trump’s lawyers asked a court to move slowly in reviewing his claims of presidential immunity.The decision to freeze the case came as former President Trump’s lawyers asked a court to move slowly in reviewing his claims of presidential immunity.",
     categories: ["politics", "lifestyle", "gastronomy"],
     location: "International",
     language: "Romanian",
@@ -14,40 +14,38 @@ export default function TopNews() {
   };
 
   return (
-    <div className="container-xl col-sm-12 border-bottom border-danger">
+    <div className="container-xl col-sm-12">
       <a
+        className=""
         href={`/news/read/${testObject.id}`}
         style={{
           textDecoration: "none",
           color: "black",
         }}
       >
-        <div className="d-xl-flex align-items-center">
+        <div className="d-xl-flex align-items-center border-bottom border-danger">
           <img
-            className="img-fluid col-xl-6 col-sm-12 mb-3"
+            className="img-fluid col-xl-5 col-sm-12 mb-3"
             src="https://www.state.gov/wp-content/uploads/2022/01/shutterstock_248799484-scaled.jpg"
-            style={{ borderRadius: 16, marginRight: "5px" }}
+            style={{ borderRadius: 16 }}
           />
-          <div className="col-xl-6 col-sm-12">
+          <div className="col-xl-7 col-sm-12">
             <h3>{testObject.title}</h3>
             <p>{testObject.short}</p>
             <h5>Categories:</h5>
             {testObject.categories.map((categ) => (
               <a
-                type="button"
-                className="btn btn-outline-success btn-sm ms-2 my-3"
-                data-mdb-ripple-init
-                data-mdb-ripple-color="dark"
+                href={`/news/category/${categ}`}
+                className="btn btn-outline-success btn-sm ms-2 my-2"
                 style={{
                   borderRadius: 10,
+                  fontSize:15
                 }}
-                href={`/news/category/${categ}`}
-                disabled
               >
-                <h5>{FirstLetterUppercase(categ)}</h5>
+                {FirstLetterUppercase(categ)}
               </a>
             ))}
-            <h7 className="d-flex justify-content-end mb-3">Posted at:</h7>
+            <h6 className="d-flex justify-content-end mb-2">Posted at:</h6>
           </div>
         </div>
       </a>
--- Frontend/src/usefull/FirstLetterUppercase.js
+++ Frontend/src/usefull/FirstLetterUppercase.js
@@ -1,4 +1,3 @@
 export default function FirstLetterUppercase( category ) {
-console.log(category)
   return category.charAt(0).toUpperCase() + category.slice(1).toLowerCase();
 }
