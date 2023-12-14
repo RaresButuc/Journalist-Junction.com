3a063d1b5dec6a0c82823cd3bddc5ecbd246633e
RaresButuc <eusuntbutuc01@gmail.com>
2023-12-14 17:06:02 +0200

    Categories added

--- Frontend/src/components/TopNews.js
+++ Frontend/src/components/TopNews.js
@@ -6,7 +6,7 @@ export default function TopNews() {
     title: "Titlu de Articol",
     short:
       "The decision to freeze the case came as former President Trump’s lawyers asked a court to move slowly in reviewing his claims of presidential immunity.",
-    categories: ["politics", "lifestyle", "gastronomy", "sport"],
+    categories: ["politics", "lifestyle", "gastronomy"],
     location: "International",
     language: "Romanian",
     ownerName: "Rares",
@@ -26,26 +26,31 @@ export default function TopNews() {
           <img
             className="img-fluid col-xl-6 col-sm-12 mb-3"
             src="https://www.state.gov/wp-content/uploads/2022/01/shutterstock_248799484-scaled.jpg"
-            style={{ borderRadius: 16, marginLeft: "10px" }}
+            style={{ borderRadius: 16, marginRight: "5px" }}
           />
           <div className="col-xl-6 col-sm-12">
             <h3>{testObject.title}</h3>
             <p>{testObject.short}</p>
+            <h5>Categories:</h5>
+            {testObject.categories.map((categ) => (
+              <a
+                type="button"
+                className="btn btn-outline-success btn-sm ms-2 my-3"
+                data-mdb-ripple-init
+                data-mdb-ripple-color="dark"
+                style={{
+                  borderRadius: 10,
+                }}
+                href={`/news/category/${categ}`}
+                disabled
+              >
+                <h5>{FirstLetterUppercase(categ)}</h5>
+              </a>
+            ))}
+            <h7 className="d-flex justify-content-end mb-3">Posted at:</h7>
           </div>
         </div>
       </a>
-
-      {testObject.categories.map((categ) => (
-        <button
-          className="btn btn-outline-success m-2"
-          style={{
-            borderRadius: 10,
-          }}
-          href={`/news/category/${categ}`}
-        >
-          <h5>{FirstLetterUppercase(categ)}</h5>
-        </button>
-      ))}
     </div>
   );
 }
--- Frontend/src/pages/HomePage.js
+++ Frontend/src/pages/HomePage.js
@@ -3,11 +3,11 @@ import TopNews from "../components/TopNews";
 export default function HomePage() {
   return (
     <div className="row">
-      <div className="col-7">
+      <div className="col-xl-8 col-sm-12">
         <TopNews />
       </div>
 
-      <div className="col-5">wow</div>
+      <div className="col-xl-4 col-sm-12">wow</div>
     </div>
   );
 }
