b34821ae951b21d938cf9f367cae70ad6317ddf9
RaresButuc <eusuntbutuc01@gmail.com>
2023-12-20 10:13:26 +0200

    Authentication Request

--- Backend/src/main/java/com/journalistjunction/configuration/CorsConfig.java
+++ Backend/src/main/java/com/journalistjunction/configuration/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.journalistjunction.cors;
+package com.journalistjunction.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
