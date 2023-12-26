c83aae5d39450ee706de61223bf8a5cb8de5a1e2
RaresButuc <eusuntbutuc01@gmail.com>
2023-12-27 01:12:37 +0200

    Subsribe Button Chosen and Number system fixed

--- Frontend/src/pages/ProfilePage.js
+++ Frontend/src/pages/ProfilePage.js
@@ -59,6 +59,26 @@ export default function ProfilePage() {
     }
   }, [auth()?.email, id, subsCount]);
 
+  function formatNumber(number) {
+    if (number < 1000) {
+      return number.toString();
+    } else if (number < 1000000) {
+      const remainder = number % 1000;
+      if (remainder === 0) {
+        return Math.floor(number / 1000) + "K";
+      } else {
+        return (number / 1000).toString().substring(0, 3) + "K";
+      }
+    } else {
+      const remainder = number % 1000000;
+      if (remainder === 0) {
+        return Math.floor(number / 1000000) + "M";
+      } else {
+        return (number / 1000000).toString().substring(0, 3) + "M";
+      }
+    }
+  }
+
   const subscribeOrUnsubscribe = () => {};
 
   return (
@@ -85,20 +105,8 @@ export default function ProfilePage() {
         <h1>{profileUser?.name}</h1>
         {/* Subscribe Button */}
         <div className="d-flex justify-content-center">
-          <button className="Btn">
-            <span className="leftContainer">
-              <svg
-                fill="white"
-                viewBox="0 0 512 512"
-                height="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"></path>
-              </svg>
-              {/* <span className={subButtonContent[0]}>{subButtonContent[1]}</span> */}
-              <span className="like">Like</span>
-            </span>
-            <span className="subscribeCount">{subsCount}</span>
+          <button type="button" class="btn btn-danger mr-md-3 mb-2 mb-md-0">
+            {subButtonContent[1]} - {formatNumber(subsCount)}
           </button>
         </div>
 
