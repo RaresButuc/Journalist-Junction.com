f76509ea56419dfefdcfc25aad1be0e4dbc3de72
RaresButuc <eusuntbutuc01@gmail.com>
2023-12-29 13:37:37 +0200

    Country Select Works and Register Page Style Finished

--- Frontend/src/components/formComponents/CountrySelect.js
+++ Frontend/src/components/formComponents/CountrySelect.js
@@ -1,41 +1,60 @@
-import { forwardRef, useState, useEffect } from "react";
+// components/formComponents/CountrySelect.js
+import React, { forwardRef, useState, useEffect } from "react";
 import axios from "axios";
+import Select from "react-select";
 
-function NameInput({ user }, ref) {
+function CountrySelect({ user }, ref) {
   const [allCountries, setAllCountries] = useState([]);
 
   useEffect(() => {
-    const allCountries = async () => {
-      const response = await axios.get("https://restcountries.com/v3.1/all");
-      const data = response.data.map((e) => e.name.common).sort();
+    const fetchCountries = async () => {
+      try {
+        const response = await axios.get("https://restcountries.com/v3.1/all");
+        const dataCountries = response.data.map((country) => ({
+          value: country.name.common,
+          label: (
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <img
+                className="mx-2 mb-1"
+                src={`https://flagsapi.com/${country.cca2}/flat/16.png`}
+                alt={country.name.common}
+              />
+              {country.name.common}
+            </div>
+          ),
+        }));
 
-      setAllCountries(data);
+        const sortedCountries = dataCountries.sort((a, b) =>
+          a.value.localeCompare(b.value)
+        );
+        setAllCountries(sortedCountries);
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
     };
 
-    allCountries();
+    fetchCountries();
   }, []);
 
   return (
-    <div>
-      <select
-        ref={ref}
-        className="form-control"
-        name="countryInput"
-        defaultValue={user?.country}
-        required
-      >
-        <option disabled selected value={user ? user?.country : ""}>
-          {user ? user?.country : "Select Your Residence Country"}
-        </option>
-        {allCountries &&
-          allCountries.map((country, index) => (
-            <option value={country} key={index}>
-              {country}
-            </option>
-          ))}
-      </select>
-    </div>
+    <Select
+      name="countryInput"
+      ref={ref}
+      options={allCountries}
+      defaultValue={{
+        label: user ? user.country : "Select Your Residence Country",
+        value: user ? user.country : "",
+      }}
+      menuPortalTarget={document.body}
+      styles={{
+        menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+        control: (baseStyles, state) => ({
+          ...baseStyles,
+          borderColor: state.isFocused ? "grey" : "red",
+        }),
+      }}
+    />
   );
 }
 
-export default forwardRef(NameInput);
+export default forwardRef(CountrySelect);
