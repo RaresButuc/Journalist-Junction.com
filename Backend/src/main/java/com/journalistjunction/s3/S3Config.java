493e9219c5b2605d4bcac25f3bf4d6d7199e4420
RaresButuc <eusuntbutuc01@gmail.com>
2024-03-11 15:08:02 +0200

    AWS Backend Implemented Basic

---
+++ Backend/src/main/java/com/journalistjunction/s3/S3Config.java
@@ -0,0 +1,22 @@
+package com.journalistjunction.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public S3Client s3Client() {
+        S3Client client = S3Client.builder()
+                .region(Region.of(awsRegion))
+                .build();
+
+        return client;
+    }
+}
